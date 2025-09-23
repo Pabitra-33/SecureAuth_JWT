71bb83577f791ad123a538055b17ba65fe40ee16
Pabitra-33 <kumarpabitra345@gmail.com>
2025-09-23 22:38:06 +0530

    first commit

---
+++ src/main/java/security_for_login_registration/JwtSecurityLoginRegistrationApplication.java
@@ -0,0 +1,13 @@
+package security_for_login_registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtSecurityLoginRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JwtSecurityLoginRegistrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/security_for_login_registration/JwtSecurityLoginRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package security_for_login_registration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtSecurityLoginRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
