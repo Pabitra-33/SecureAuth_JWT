cb30e7d536d712d671bbda8bad35e99dd08a390e
Pabitra-33 <kumarpabitra345@gmail.com>
2025-09-24 23:29:45 +0530

    created a entity class with required attributes

---
+++ src/main/java/security_for_login_registration/entity/Customer.java
@@ -0,0 +1,27 @@
+package security_for_login_registration.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer Cid;
+	private String Cname;
+	private Integer age;
+	private String email;
+	private String pswd;
+	private long phone;
+}
